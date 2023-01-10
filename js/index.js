f2a2c7676a168d3d08dbef49a490e6a7e67020b9
katsuro666 <kreepinoutdakut@gmail.com>
2023-01-10 18:00:02 +0300

    feat: add js to mobile nav menu

--- js/index.js
+++ js/index.js
@@ -1,1 +1,17 @@
 console.log('110/110')
+
+let navBurger = document.querySelector('.header__burger')
+let navMenu = document.querySelector('.header__nav')
+let navCross = document.querySelector('.nav__cross')
+
+function toggleMenu() {
+  navMenu.classList.toggle('header__nav--active')
+}
+
+navBurger.addEventListener('click', toggleMenu)
+navCross.addEventListener('click', toggleMenu)
+window.addEventListener('click', (e) => {
+  if (e.target !== navBurger) {
+    navMenu.classList.remove('header__nav--active')
+  }
+})
